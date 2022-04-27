eeb68c8e5fdd0794d0bd16b152a313a9d1aa898a
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2022-04-28 01:40:28 +0530

    Create SimpleArraySum.py

---
+++ SimpleArraySum.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    #TAke two empty variables first
+    boxA=0
+    boxB=0
+    #start a loop:
+    for i in range(ar_count):
+        boxA=ar[i]
+        boxB=boxB+boxA
+        i+=1
+    return boxB
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
